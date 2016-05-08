938e6690691bfccf3f96ead1c2f86bc0397b042a
tejvuligonda <tejvuligonda@gmail.com>
2016-05-08 10:07:36 -0700

    Created simple login page that uses AutoLayout

---
+++ Fluid-Savings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fluid-Savings
+//
+//  Created by Tej Vuligonda on 4/10/16.
+//  Copyright © 2016 Tej Vuligonda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
