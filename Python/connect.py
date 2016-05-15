4c705a322d25b5b0a19486c4ccb090afbbbc67a6
tejvuligonda <tejvuligonda@gmail.com>
2016-05-15 13:25:05 -0700

    Tested connection using MySQLdb library in Python, connection successful to localhost

--- Python/connect.py
+++ Python/connect.py
@@ -1,6 +1,8 @@
 import sqlite3
+import MySQLdb
 
-connection = sqlite3.connect(host="192.168.1.167")
+connection = MySQLdb.connect(host="192.168.1.167",user="root",passwd="1234",db="test1")
+#connection = sqlite3.connect("192.168.1.167")
 
 c = connection.cursor()
 print("Connection Success")
