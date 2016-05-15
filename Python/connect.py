0b97fba3c46131d28215cff5ca18bae1d71ea0c2
tejvuligonda <tejvuligonda@gmail.com>
2016-05-15 13:39:49 -0700

    Updated Python script to print out all data in the database

--- Python/connect.py
+++ Python/connect.py
@@ -1,8 +1,13 @@
 import sqlite3
 import MySQLdb
 
-connection = MySQLdb.connect(host="192.168.1.167",user="root",passwd="1234",db="test1")
+connection = MySQLdb.connect(host="192.168.1.167",user="root",passwd="1234",db="anotherone")
 #connection = sqlite3.connect("192.168.1.167")
 
 c = connection.cursor()
 print("Connection Success")
+
+c.execute('SELECT * FROM users')
+
+for row in c.fetchall():
+	print(row)
