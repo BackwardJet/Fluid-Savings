f8a0c0441aa78c64f39f15d32c13e1b27940e319
tejvuligonda <tejvuligonda@gmail.com>
2016-05-17 18:06:10 -0700

    Created Database class in Python, successfully connected

--- Python/connect.py
+++ Python/connect.py
@@ -1,6 +1,9 @@
 import sqlite3
 import MySQLdb
 
+login_user = "sql3119660"
+login_password = "SLeidVsdzP"
+
 connection = MySQLdb.connect(host="sql3.freemysqlhosting.net",user="sql3119660",passwd="SLeidVsdzP",db="sql3119660")
 #connection = sqlite3.connect("192.168.1.167")
 
@@ -11,3 +14,16 @@ c.execute('SELECT * FROM users')
 
 for row in c.fetchall():
 	print(row)
+
+class Database:
+	def __init__(self,login_user,login_password):
+		self.connection = MySQLdb.connect(host= "sql3.freemysqlhosting.net",user = login_user, passwd = login_password,db = "sql3119660")
+		self.databaseName = "sql3119660"
+		self.tableNameUsers = "users"
+		self.tableNameUsage = "watermeterusage"
+		print ("Connection Sucess")
+		
+
+database = Database(login_user,login_password)
+
+
