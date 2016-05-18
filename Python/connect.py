33a8c2a03e670833b7bbdee72ea684a01e5c5ab3
tejvuligonda <tejvuligonda@gmail.com>
2016-05-17 18:13:52 -0700

    Added a Python function to print all users in database

--- Python/connect.py
+++ Python/connect.py
@@ -4,17 +4,6 @@ import MySQLdb
 login_user = "sql3119660"
 login_password = "SLeidVsdzP"
 
-connection = MySQLdb.connect(host="sql3.freemysqlhosting.net",user="sql3119660",passwd="SLeidVsdzP",db="sql3119660")
-#connection = sqlite3.connect("192.168.1.167")
-
-c = connection.cursor()
-print("Connection Success")
-
-c.execute('SELECT * FROM users')
-
-for row in c.fetchall():
-	print(row)
-
 class Database:
 	def __init__(self,login_user,login_password):
 		self.connection = MySQLdb.connect(host= "sql3.freemysqlhosting.net",user = login_user, passwd = login_password,db = "sql3119660")
@@ -22,8 +11,13 @@ class Database:
 		self.tableNameUsers = "users"
 		self.tableNameUsage = "watermeterusage"
 		print ("Connection Sucess")
-		
+
+	def printAllUsers(self):
+		c = self.connection.cursor() 		
+		c.execute('SELECT * FROM ' + self.tableNameUsage)
+		for row in c.fetchall():
+			print(row[0])
 
 database = Database(login_user,login_password)
-
+database.printAllUsers()
 
